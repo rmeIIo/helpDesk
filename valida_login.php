f6b767114defabdd5d4012131764d0d1d87f1152
Roger Correa <j.rogercorreamello@gmail.com>
2024-01-15 18:16:35 -0300

    inicio do projeto e autenticação do usuario

---
+++ valida_login.php
@@ -0,0 +1,26 @@
+<?php
+
+    $usuario_autenticado = false;
+
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+    );
+
+    /*echo '<pre>';
+    print_r($usuarios_app);
+    echo '</pre>';*/
+
+    foreach($usuarios_app as $user) {
+
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuario autenticado';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+?>
