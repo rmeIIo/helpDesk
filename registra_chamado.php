a7bdd1d0ae1f7ab95be2d09ba1c6e41f948a3418
Roger Correa <j.rogercorreamello@gmail.com>
2024-01-24 14:42:00 -0300

    consulta e registro de chamados

---
+++ registra_chamado.php
@@ -0,0 +1,18 @@
+<?php 
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);    
+    
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+    $arquivo = fopen('arquivo.hd', 'a');
+    fwrite($arquivo, $texto);
+    fclose($arquivo);
+
+    header('Location: abrir_chamado.php');
